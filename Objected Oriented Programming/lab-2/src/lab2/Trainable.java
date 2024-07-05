3d26b5cb3b65ee59782f3808bd67a8578a84d517
johan023 <89706763+johan023@users.noreply.github.com>
2024-07-05 13:52:35 +0200

    Delete Objected Oriented Programming directory

--- Objected Oriented Programming/lab-2/src/lab2/Trainable.java
+++
@@ -1,6 +0,0 @@
-package lab2;
-
-public interface Trainable {
-    public void trainShooting();
-    public void trainDribbling();
-}
--- Objected Oriented Programming/lab-2/src/lab2/WatchableMovie.java
+++
@@ -1,7 +0,0 @@
-package lab2;
-
-public interface WatchableMovie {
-	public void watch(double rating);
-    public int getNumberOfViews();
-    public double getHighestRating();
-}
